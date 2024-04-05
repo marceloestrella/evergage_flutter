02551f31175f343d99613087333c7bbccfc8289b
Gabriel Isaac Corrales Escorcia <gcorrales@bnext.com>
2024-04-05 17:23:13 -0600

    Update CHANGELOG.md, main.dart, and pubspec.yaml for Evergage SDK update to version 1.4.0 and addition of new event (login)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
